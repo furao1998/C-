df98b25471a88e727166298782033ca9233b66d3
unknown <>
2017-03-06 20:26:27 +0800

    Signed-off-by: unknown <付娆>

---
+++ 向C语言问好/向C语言问好/向C语言问好.c
@@ -0,0 +1,13 @@
+/**
+ *说明：任务01_1向C语言问好
+ *姓名：付娆
+ *学号：2016011558
+ *班级：5班
+ *日期：2017/03/03
+ */
+#include <stdio.h>
+int main(void)
+{
+    printf("Nice to meet you, C! I am Furao.\n");
+    return 0;
+}
